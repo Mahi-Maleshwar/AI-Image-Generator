f5c0c93d369e06e1d6cdcce28955fb3ea98c8a58
Mahi-Maleshwar <mahimaleshwarkit@gmail.com>
2025-05-26 20:11:02 +0530

    server env changes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ await connectDB()
 
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ai-image-generator-mu-hazel.vercel.app",
   credentials: true, // only if you're using cookies/auth headers
 }));
 
