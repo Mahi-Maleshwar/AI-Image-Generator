36eb6f188eb086aa75dc7092d4d5275f85804059
Mahi-Maleshwar <mahimaleshwarkit@gmail.com>
2025-05-27 17:07:24 +0530

    chan 094

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -53,7 +53,7 @@ const BuyCredit = () => {
       const { data } = await axios.post(`${backendUrl}/api/user/pay-razor`, { planId, userId: user._id}, {headers: {
       Authorization: `Bearer ${token}` 
     }})
-
+    console.log(data)
       if (data.success){
         initPay(data.order)
       }
